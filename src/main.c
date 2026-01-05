6d8d9b765229bcffe86da142bf3a9e586a9155c6
stormtomten <80994025+stormtomten@users.noreply.github.com>
2026-01-05 20:42:39 +0100

    First repl-loop test

--- src/main.c
+++ src/main.c
@@ -5,15 +5,17 @@ int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  // TODO: Uncomment the code below to pass the first stage
-  printf("$ ");
+  while (1) {
+    // TODO: Uncomment the code below to pass the first stage
+    printf("$ ");
 
-  char command[1024];
-  fgets(command, sizeof(command), stdin);
-  // Remove the trailing newline and replace it with a '\0'
-  command[strcspn(command, "\n")] = '\0';
+    char command[1024];
+    fgets(command, sizeof(command), stdin);
+    // Remove the trailing newline and replace it with a '\0'
+    command[strcspn(command, "\n")] = '\0';
 
-  printf("%s: command not found\n", command);
+    printf("%s: command not found\n", command);
+  }
 
   return 0;
 }
