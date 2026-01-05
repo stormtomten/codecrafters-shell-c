6e6dcfcbcec245f3e629bbf499ec896958cfff54
stormtomten <80994025+stormtomten@users.noreply.github.com>
2026-01-05 20:51:18 +0100

    First try exit.

--- src/main.c
+++ src/main.c
@@ -3,7 +3,8 @@
 
 int main(int argc, char *argv[]) {
 
-  while (1) {
+  int run = 1;
+  while (run) {
     // Flush after every printf
     setbuf(stdout, NULL);
     // TODO: Uncomment the code below to pass the first stage
@@ -13,6 +14,11 @@ int main(int argc, char *argv[]) {
     fgets(command, sizeof(command), stdin);
     // Remove the trailing newline and replace it with a '\0'
     command[strcspn(command, "\n")] = '\0';
+
+    if (command == "exit") {
+      run = 0;
+      break;
+    }
 
     printf("%s: command not found\n", command);
   }
